c3b7accf5de683e51c871302812a1dee399e1983
vvidhukrishna <118620137+vvidhukrishna@users.noreply.github.com>
2023-01-22 21:12:30 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
 from PyQt5 import QtGui, QtWidgets
 from task_1 import Ui_MainWindow_
 from task_2 import Ui_MainWindow__
+from toturial import Ui_MainWindow___
 
 
 class Ui_MainWindow(object):
@@ -17,6 +19,12 @@ class Ui_MainWindow(object):
         self.ui2.setupUi(self.window3)
         self.window3.show()
 
+    def task__3(self):
+        self.window4 = QtWidgets.QMainWindow()
+        self.ui2 = Ui_MainWindow___()
+        self.ui2.setupUi(self.window4)
+        self.window4.show()
+
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
         MainWindow.resize(1280, 720)
@@ -26,16 +34,18 @@ class Ui_MainWindow(object):
         self.centralwidget.setObjectName("centralwidget")
 
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(270, 560, 101, 41)
-        self.pushButton.setObjectName("Task 1 ")
+        self.pushButton.setGeometry(270, 400, 101, 41)
+        self.pushButton.setStyleSheet(
+            "background-color:#42f5e0;border-radius:10%;font-size: 14px;font-weight: bold;color: #003d69")
+        self.pushButton.setObjectName("Easy")
         self.pushButton.clicked.connect(self.task__1)
         self.pushButton.clicked.connect(MainWindow.close)
 
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(910, 560, 101, 41)
-        self.pushButton_2.setStyleSheet("background-color:#42f5e0;border-radius:10%")
-        self.pushButton.setStyleSheet("background-color:#42f5e0;border-radius:10%")
-        self.pushButton_2.setObjectName("Task 2")
+        self.pushButton_2.setGeometry(905, 400, 101, 41)
+        self.pushButton_2.setStyleSheet(
+            "background-color:#42f5e0;border-radius:10%;font-size: 14px;font-weight: bold;color: #003d69")
+        self.pushButton_2.setObjectName("Hard")
         self.pushButton_2.clicked.connect(self.task__2)
         self.pushButton_2.clicked.connect(MainWindow.close)
 
@@ -47,8 +57,45 @@ class Ui_MainWindow(object):
 
         self.pushButton.raise_()
         self.pushButton_2.raise_()
-        self.pushButton.setText("TASK 1 ")
-        self.pushButton_2.setText("TASK 2")
+        self.pushButton.setText("EASY")
+        self.pushButton_2.setText("HARD")
+
+        self.label_1 = QtWidgets.QLabel(self.centralwidget)
+        self.label_1.setGeometry(517, 390, 250, 100)
+        self.label_1.setText("""
+In this quest you will have to match the number in your movable pink tile to that of the white tile on the pillar of answers. You may obtain your target by using the tiles beside you.
+        """)
+        self.label_1.setStyleSheet("background-color:#037ba7;border-radius:10%;color: white;font-size: 13px;")
+        self.label_1.setAlignment(Qt.AlignCenter)
+        self.label_1.setWordWrap(True)
+        self.label_1.setObjectName("label1")
+
+        self.label_2 = QtWidgets.QLabel(self.centralwidget)
+        self.label_2.setGeometry(483, 120, 320, 40)
+        self.label_2.setText('WELCOME TO THE TEMPLE OF OPERATIONS')
+        self.label_2.setStyleSheet("background-color:#004167;border-radius:10%; font-weight: bold;color: white;font-size: 14px;")
+        self.label_2.setAlignment(Qt.AlignCenter)
+        self.label_2.setWordWrap(True)
+        self.label_2.setObjectName("label1")
+
+        self.pushButton_3 = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton_3.setGeometry(523, 510, 101, 41)
+        self.pushButton_3.setStyleSheet(
+            "background-color:#42f5e0;border-radius:10%; font-weight: bold;color: #003d69;font-size: 14px;")
+        self.pushButton_3.setObjectName("Tutorial")
+        self.pushButton_3.clicked.connect(self.task__3)
+        self.pushButton_3.clicked.connect(MainWindow.close)
+        self.pushButton_3.raise_()
+        self.pushButton_3.setText("Tutorial")
+
+        self.label_3 = QtWidgets.QLabel(self.centralwidget)
+        self.label_3.setGeometry(663, 510, 101, 41)
+        self.label_3.setText('GOOD LUCK')
+        self.label_3.setStyleSheet(
+            "background-color:#42f5e0;border-radius:10%; font-weight: bold;color: #003d69;font-size: 14px;")
+        self.label_3.setAlignment(Qt.AlignCenter)
+        self.label_3.setWordWrap(True)
+        self.label_3.setObjectName("label1")
 
         MainWindow.setCentralWidget(self.centralwidget)
 
--- toturial.py
+++ toturial.py
@@ -3,28 +3,32 @@ from PyQt5.QtCore import *
 from PyQt5.QtGui import *
 from PyQt5.QtWidgets import QMessageBox
 from PyQt5 import QtGui, QtWidgets
-from algoritham import values_and_target
+from task_1 import Ui_MainWindow_
 
-class Ui_MainWindow__(object):
+
+class Ui_MainWindow___(object):
     def __init__(self):
-        self.Difficulty = 6
         self.moving_value = 1
-        self.values, self.target = values_and_target(8, self.Difficulty)
-        self.target = str(int(float(self.target)))
-        print(self.target)
+
+    def task__1(self):
+        self.window3 = QtWidgets.QMainWindow()
+        self.ui2 = Ui_MainWindow_()
+        self.ui2.setupUi(self.window3)
+        self.window3.show()
 
     def win_message(self):
         msg = QMessageBox()
         msg.setWindowTitle("Victory Box!")
         msg.setText("YOU WON!!!")
         msg.setIcon(QMessageBox.Information)
-        msg.setStandardButtons(QMessageBox.Close)
+        msg.setStandardButtons(QMessageBox.Ok)
         msg.buttonClicked.connect(self.reply)
         msg.exec_()
 
     def reply(self, i):
-        if i.text() == 'Close':
-            exit()
+        if i.text() == 'OK':
+            self.task__1()
+
 
     def animation(self):
         self.animation = QPropertyAnimation(self.movingbutton, b'geometry')
@@ -37,122 +41,17 @@ class Ui_MainWindow__(object):
         self.animation1.setDuration(1000)
         self.animation1.setEndValue(QRect(self.pushButton1.x(), self.pushButton1.y(), 90, 90))
         self.animation1.start()
-        self.moving_value = round(eval(str(self.moving_value) +self.values[0]))
+        self.moving_value = round(eval(str(self.moving_value) + '+ 3'))
         print(self.moving_value)
-        print(self.target)
+        print(4)
         self.movingbutton.setText(str(self.moving_value))
         self.pushButton1.setEnabled(False)
-        if int(self.moving_value) == int(self.target):
+        if int(self.moving_value) == int('4'):
             self.win_message()
-
-    def animation2(self):
-        self.animation2 = QPropertyAnimation(self.movingbutton, b'geometry')
-        self.animation2.setDuration(1000)
-        self.animation2.setEndValue(QRect(self.pushButton2.x(), self.pushButton2.y(), 90, 90))
-        self.animation2.start()
-        self.moving_value = round(eval(str(self.moving_value) +self.values[1]))
-        print(self.moving_value)
-        self.movingbutton.setText(str(self.moving_value))
-        self.pushButton2.setEnabled(False)
-        if int(self.moving_value) == int(self.target):
-            self.win_message()
-
-    def animation3(self):
-        self.animation3 = QPropertyAnimation(self.movingbutton, b'geometry')
-        self.animation3.setDuration(1000)
-        self.animation3.setEndValue(QRect(self.pushButton3.x(), self.pushButton3.y(), 90, 90))
-        self.animation3.start()
-        self.moving_value = round(eval(str(self.moving_value) +self.values[2]))
-        print(self.moving_value)
-        self.movingbutton.setText(str(self.moving_value))
-        self.pushButton3.setEnabled(False)
-        if int(self.moving_value) == int(self.target):
-            self.win_message()
-
-    def animation4(self):
-        self.animation4 = QPropertyAnimation(self.movingbutton, b'geometry')
-        self.animation4.setDuration(1000)
-        self.animation4.setEndValue(QRect(self.pushButton4.x(), self.pushButton4.y(), 90, 90))
-        self.animation4.start()
-        self.moving_value = round(eval(str(self.moving_value) + self.values[3]))
-        print(self.moving_value)
-        self.movingbutton.setText(str(self.moving_value))
-        self.pushButton4.setEnabled(False)
-        if int(self.moving_value) == int(self.target):
-            self.win_message()
-
-    def animation5(self):
-        self.animation5 = QPropertyAnimation(self.movingbutton, b'geometry')
-        self.animation5.setDuration(1000)
-        self.animation5.setEndValue(QRect(self.pushButton5.x(), self.pushButton5.y(), 90, 90))
-        self.animation5.start()
-        if int(self.moving_value) == int(self.target):
-            self.win_message()
-
-    def animation6(self):
-        self.animation6 = QPropertyAnimation(self.movingbutton, b'geometry')
-        self.animation6.setDuration(1000)
-        self.animation6.setEndValue(QRect(self.pushButton6.x(), self.pushButton6.y(), 90, 90))
-        self.animation6.start()
-        self.moving_value = round(eval(str(self.moving_value) + self.values[4]))
-        print(self.moving_value)
-        self.movingbutton.setText(str(self.moving_value))
-        self.pushButton6.setEnabled(False)
-        if int(self.moving_value) == int(self.target):
-            self.win_message()
-
-    def animation7(self):
-        self.animation7 = QPropertyAnimation(self.movingbutton, b'geometry')
-        self.animation7.setDuration(1000)
-        self.animation7.setEndValue(QRect(self.pushButton7.x(), self.pushButton7.y(), 90, 90))
-        self.animation7.start()
-        self.moving_value = round(eval(str(self.moving_value) + self.values[5]))
-        print(self.moving_value)
-        self.movingbutton.setText(str(self.moving_value))
-        self.pushButton7.setEnabled(False)
-        if int(self.moving_value) == int(self.target):
-            self.win_message()
-
-    def animation8(self):
-        self.animation8 = QPropertyAnimation(self.movingbutton, b'geometry')
-        self.animation8.setDuration(1000)
-        self.animation8.setEndValue(QRect(self.pushButton8.x(), self.pushButton8.y(), 90, 90))
-        self.animation8.start()
-        self.moving_value = round(eval(str(self.moving_value) + self.values[6]))
-        print(self.moving_value)
-        self.movingbutton.setText(str(self.moving_value))
-        self.pushButton8.setEnabled(False)
-        if int(self.moving_value) == int(self.target):
-            self.win_message()
-
-    def animation9(self):
-        self.animation9 = QPropertyAnimation(self.movingbutton, b'geometry')
-        self.animation9.setDuration(1000)
-        self.animation9.setEndValue(QRect(self.pushButton9.x(), self.pushButton9.y(), 90, 90))
-        self.animation9.start()
-        self.moving_value = round(eval(str(self.moving_value) + self.values[7]))
-        print(self.moving_value)
-        self.movingbutton.setText(str(self.moving_value))
-        self.pushButton9.setEnabled(False)
-        if int(self.moving_value) == int(self.target):
-            self.win_message()
-
-    def StartOver(self):
-        self.pushButton1.setEnabled(True)
-        self.pushButton2.setEnabled(True)
-        self.pushButton3.setEnabled(True)
-        self.pushButton4.setEnabled(True)
-        self.pushButton6.setEnabled(True)
-        self.pushButton7.setEnabled(True)
-        self.pushButton8.setEnabled(True)
-        self.pushButton9.setEnabled(True)
-        self.moving_value = 1
-        self.movingbutton.setGeometry(260, 360, 90, 90)
-        self.movingbutton.setText(str(self.moving_value))
 
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.setWindowTitle("TASK 2")
+        MainWindow.setWindowTitle("TASK 1")
         MainWindow.resize(1280, 720)
         MainWindow.setWindowIcon(QtGui.QIcon('Pc_builder_logo.ico'))
         self.centralwidget = QtWidgets.QWidget(MainWindow)
@@ -167,13 +66,13 @@ class Ui_MainWindow__(object):
         self.req_output_display = QtWidgets.QLabel(self.centralwidget)
         self.req_output_display.setGeometry(1080, 360, 90, 90)
         self.req_output_display.setStyleSheet("background:white;")
-        self.req_output_display.setText(self.target)
+        self.req_output_display.setText('4')
         self.req_output_display.setAlignment(Qt.AlignCenter)
         self.req_output_display.setObjectName("req_output_display")
         self.req_output_display.setStyleSheet("background-color:pink;border-radius:10%;font-size:20px")
 
         self.label_20 = QtWidgets.QLabel(self.centralwidget)
-        self.label_20.setText("Solve the mystery of the ancient temple")
+        self.label_20.setText("Click + 3 to reach the target")
         self.label_20.setGeometry(630, 40, 361, 91)
         self.label_20.setStyleSheet("font-size:30px;")
         self.label_20.adjustSize()
@@ -181,7 +80,6 @@ class Ui_MainWindow__(object):
         self.label_20.setObjectName("label_20")
 
         self.startbutton = QtWidgets.QPushButton(self.centralwidget)
-        self.startbutton.clicked.connect(self.StartOver)
         self.startbutton.setGeometry(810, 530, 151, 31)
         self.startbutton.setObjectName("startbutton")
         self.startbutton.setText('START OVER')
@@ -197,63 +95,55 @@ class Ui_MainWindow__(object):
         self.pushButton1 = QtWidgets.QPushButton(self.centralwidget)
         self.pushButton1.clicked.connect(self.animation1)
         self.pushButton1.setGeometry(150, 250, 90, 90)
-        self.pushButton1.setText(self.values[0])
+        self.pushButton1.setText('+ 3')
         self.pushButton1.setObjectName("pushButton1")
         self.pushButton1.setStyleSheet("background-color:white;border-radius:10%;font-size:20px;")
 
         self.pushButton2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton2.clicked.connect(self.animation2)
         self.pushButton2.setGeometry(260, 250, 90, 90)
-        self.pushButton2.setText(self.values[1])
+        self.pushButton2.setText('')
         self.pushButton2.setObjectName("pushButton2")
         self.pushButton2.setStyleSheet("background-color:white;border-radius:10%;font-size:20px;")
 
         self.pushButton3 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton3.clicked.connect(self.animation3)
         self.pushButton3.setGeometry(370, 250, 90, 90)
-        self.pushButton3.setText(self.values[2])
+        self.pushButton3.setText('')
         self.pushButton3.setObjectName("pushButton3")
         self.pushButton3.setStyleSheet("background-color:white;border-radius:10%;font-size:20px;")
 
         self.pushButton4 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton4.clicked.connect(self.animation4)
         self.pushButton4.setGeometry(150, 360, 90, 90)
-        self.pushButton4.setText(self.values[3])
+        self.pushButton4.setText('')
         self.pushButton4.setObjectName("pushButton4")
         self.pushButton4.setStyleSheet("background-color:white;border-radius:10%;font-size:20px;")
 
         self.pushButton5 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton5.clicked.connect(self.animation5)
         self.pushButton5.setGeometry(260, 360, 90, 90)
         self.pushButton5.setText("")
         self.pushButton5.setObjectName("pushButton5")
         self.pushButton5.setStyleSheet("background-color:white;border-radius:10%;font-size:20px;")
 
         self.pushButton6 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton6.clicked.connect(self.animation6)
         self.pushButton6.setGeometry(370, 360, 90, 90)
-        self.pushButton6.setText(self.values[4])
+        self.pushButton6.setText('')
         self.pushButton6.setObjectName("pushButton6")
         self.pushButton6.setStyleSheet("background-color:white;border-radius:10%;font-size:20px;")
 
         self.pushButton7 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton7.clicked.connect(self.animation7)
         self.pushButton7.setGeometry(150, 470, 90, 90)
-        self.pushButton7.setText(self.values[5])
+        self.pushButton7.setText('')
         self.pushButton7.setObjectName("pushButton7")
         self.pushButton7.setStyleSheet("background-color:white;border-radius:10%;font-size:20px;")
 
         self.pushButton8 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton8.clicked.connect(self.animation8)
         self.pushButton8.setGeometry(260, 470, 90, 90)
-        self.pushButton8.setText(self.values[6])
+        self.pushButton8.setText('')
         self.pushButton8.setObjectName("pushButton8")
         self.pushButton8.setStyleSheet("background-color:white;border-radius:10%;font-size:20px;")
 
         self.pushButton9 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton9.clicked.connect(self.animation9)
         self.pushButton9.setGeometry(370, 470, 90, 90)
-        self.pushButton9.setText(self.values[7])
+        self.pushButton9.setText('')
         self.pushButton9.setObjectName("pushButton9")
         self.pushButton9.setStyleSheet("background-color:white;border-radius:10%;font-size:20px;")
 
@@ -281,7 +171,7 @@ class Ui_MainWindow__(object):
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     MainWindow = QtWidgets.QMainWindow()
-    ui = Ui_MainWindow__()
+    ui = Ui_MainWindow___()
     ui.setupUi(MainWindow)
     MainWindow.show()
     sys.exit(app.exec_())
